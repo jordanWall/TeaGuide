a2859687bee0a721986a9006f787eb1496af341c
jordwall <jordwall7@gmail.com>
2020-01-28 13:56:47 -0600

    Initial Commit

---
+++ TeaGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeaGuide
+//
+//  Created by Jordan Wall on 2020-01-28.
+//  Copyright © 2020 Jordan Wall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
